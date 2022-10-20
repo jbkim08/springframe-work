3d4cc61536020179c7f1d60d950e4e58b15a3b7b
jbkim <drv98@naver.com>
2022-10-20 09:36:42 +0900

    빈 라이프사이클2

---
+++ BeanLifeCycle/src/main/java/com/demo/beans/TestBean2.java
@@ -0,0 +1,20 @@
+package com.demo.beans;
+
+public class TestBean2 {
+
+	public TestBean2() {
+		System.out.println("TestBean2의 생성자!");
+	}
+	
+	public void default_init() {
+		System.out.println("TestBean2의 디폴트 init 메소드");
+	}
+	
+	public void default_destory() {
+		System.out.println("TestBean2의 디폴트 destroy 메소드");
+	}
+}
+
+
+
+
--- BeanLifeCycle/src/main/java/com/demo/main/MainClass.java
+++ BeanLifeCycle/src/main/java/com/demo/main/MainClass.java
@@ -3,6 +3,7 @@ package com.demo.main;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.demo.beans.TestBean1;
+import com.demo.beans.TestBean2;
 
 public class MainClass {
 
@@ -13,6 +14,10 @@ public class MainClass {
 		TestBean1 t1 = ctx.getBean("t1", TestBean1.class);
 		System.out.printf("t1 : %s\n", t1);
 		System.out.println("====================================");	
+		
+		TestBean2 t2 = ctx.getBean("t2", TestBean2.class);
+		System.out.printf("t2 : %s\n", t2);
+		
 		ctx.close();
 	}	
 }
